08366326c3d8b9aa6a8a1d60d0fdc621005e5298
emman123 <67055497+mantot-123@users.noreply.github.com>
2025-12-28 20:48:03 +0000

    feat/fix: Improved logging + overall code structure; Script now automatically discards/deletes the file in process of copying in case of an error; Moved cloud upload functions from "main" to backup manager

--- local_backup_manager.py
+++ local_backup_manager.py
@@ -3,25 +3,75 @@ import logging
 import shutil
 import zipfile
 import datetime
+import google_drive_manager
+
 from backup_config import BackupConfig
 from pathlib import Path
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class LocalBackupManager:
     def __init__(self, config: BackupConfig):
         self.config = config
-        self.set_backup_name = self.name_backup()
-        logging.basicConfig(filename=f"logs/[LOG]-{self.set_backup_name}.log", level=logging.INFO)
-        self.logger = logging.getLogger(__name__)
+        self.output_backup_name = self.name_backup()
+        self.setup_logging()
+
+    def setup_logging(self):
+        # logging.basicConfig(filename=f"logs/[LOG]-{self.output_backup_name}.log", level=logging.DEBUG)
+        file_handler = logging.FileHandler(f"logs/[LOG]-{self.output_backup_name}.log")
+        file_handler.setLevel(logging.DEBUG)
+
+        cli_handler = logging.StreamHandler()
+        cli_handler.setLevel(logging.DEBUG)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(cli_handler)
 
     def perform_backup(self) -> Path:
+        compress_path = None
         backup_path = self.copy_file()
 
+        if backup_path == None:
+            target_path = os.path.join(self.config.source_path, self.config.selected_file)
+            logger.error(f"Failed to backup {target_path}. Please check that the source file/directory exists, is named correctly and try again.")
+            return None
+
+        is_compressed = False
         if self.config.compress_backup == "1" and backup_path != None:
             compress_path = self.compress_backup(backup_path)
-            return compress_path
+            is_compressed = True
 
+        if self.config.cloud_providers:
+            if is_compressed:
+                self.upload_to_cloud(compress_path)
+            else:
+                self.upload_to_cloud(backup_path)
+
+        logger.info(f"Backup successfully created. The output file is located locally at: {str(backup_path)}")
         return backup_path
    
+    def upload_to_cloud(self, output_path):
+        current_service = ""
+        try:
+            if "google_drive" in self.config.cloud_providers:
+                current_service = "Google Drive"
+                logger.info(f"Backing up output file/folder to Google Drive: {output_path}")
+                creds = google_drive_manager.authorise()
+                google_drive_manager.upload(creds, output_path)
+
+            if "onedrive" in self.config.cloud_providers:
+                # TODO ONEDRIVE BACKUP HERE
+                pass
+            
+            if "dropbox" in self.config.cloud_providers:
+                # TODO DROPBOX BACKUP HERE
+                pass
+
+        except Exception as e:
+            error_msg = f"Error while syncing output file {output_path} to cloud service {current_service}: {e}"
+            logger.error(error_msg)
+
     # returns a timestamp in the following format:
     # <backup_date>-<24h_formatted_time>
     def add_timestamp(self) -> str:
@@ -57,51 +107,68 @@ class LocalBackupManager:
         # check source path and target file's existence
         src_path_check = self.config.check_source_path()
         if src_path_check == -1:
-            self.logger.error("Source directory does not exist")
-            raise FileNotFoundError("Error: Source path does not exist")
+            error_msg = f"Source directory {self.config.source_path} does not exist"
+            logger.error(error_msg)
+            raise FileNotFoundError(error_msg)
 
         file_check = self.config.check_file_existence()
         if file_check == -1:
-            self.logger.error("Target file does not exist")
-            raise FileNotFoundError("Error: Target file does not exist")
+            error_msg = f"Target file {self.config.selected_file} in parent directory {self.config.source_path} does not exist"
+            logger.error(error_msg)
+            raise FileNotFoundError(error_msg)
 
-        if src_path_check == 0:
+        if src_path_check == 0: # check if the source directory is specified
+            # if not specified, assume that the target file we want to backup 
+            # is in the current directory the script is running
             full_path = Path(self.config.selected_file)
         else:
             full_path = Path(os.path.join(self.config.source_path, self.config.selected_file))
 
         dest_path_check = self.config.check_dest_path()
+
         # destination directory does not exist, automatically create one
         if dest_path_check == -1:
             try:
                 os.makedirs(self.config.destination_path)
             except Exception as e:
-                self.logger.error(f"An error occurred while attempting to create the destination directory: {e}")
+                error_msg = f"An error occurred while attempting to create the destination directory: {e}"
+                logger.error(error_msg)
                 return None
 
         # copy operation here
+        new_dest_path = None
         try:
             # when using the shutil copy methods and providing it the "dest",
             # it expects the destination directory and the name the file should be after it's copied
+
             if file_check == 0: # run if we are backing up an entire directory
-                backup_name = self.set_backup_name
-                dest_dir_path = os.path.join(self.config.destination_path, backup_name)
-                shutil.copytree(full_path, dest_dir_path)
-                new_path = dest_dir_path
+                backup_name = self.output_backup_name
+                new_dest_path = os.path.join(self.config.destination_path, backup_name)
+                shutil.copytree(full_path, new_dest_path)
+                new_path = new_dest_path
             else: # run if we are trying to backup a file
-                new_name = self.set_backup_name
-                dest_file_path = os.path.join(self.config.destination_path, new_name)
-                shutil.copy2(full_path, dest_file_path)
-                new_path = dest_file_path
+                new_name = self.output_backup_name
+                new_dest_path = os.path.join(self.config.destination_path, new_name)
+                # shutil.copyfile(full_path, new_dest_path) # don't use this, it does not keep original metadata
+                shutil.copy2(full_path, new_dest_path) # this one keeps all the original metadata
+                new_path = new_dest_path
         except Exception as e:
-            self.logger.error(f"An error occurred while attempting to copy the file to the directory {self.config.destination_path}: {e}")
+            error_msg = f"An error occurred while attempting to copy the file to the directory {self.config.destination_path}: {e}"
+            logger.error(error_msg)
+
+            self.delete_copy(new_dest_path) # discards the file/directory in process in case of an error
             return None
 
-        self.logger.info(f"Successfully copied {full_path} to the new directory: {new_path}")
+        success_msg = f"Successfully copied {full_path} to the new directory: {new_path}"
+        logger.info(success_msg)
         return new_path
 
-    def discard_copy(self):
-        pass
+    # TODO
+    def delete_copy(self, path):
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        elif os.path.isfile(path):
+            os.remove(path)
 
     # (optionally, in the config file) compress the output file/folder into a ZIP file
     def compress_backup(self, backup_path) -> Path:
@@ -115,7 +182,7 @@ class LocalBackupManager:
 
         try:
             with zipfile.ZipFile(zip_name, "w") as zip_file:
-                self.logger.info(f"ZIP file: {zip_name}")
+                logger.info(f"Creating ZIP file: {zip_name}...")
                 if os.path.isdir(backup_path):
                     base_dir = os.path.basename(backup_path)
                     # os.walk() - array of tuples
@@ -132,10 +199,10 @@ class LocalBackupManager:
                             # zip_file.writestr(base_dir, arcdir) # doesn't add the empty directories
                             zip_file.writestr(arcdir, "") 
 
-                        self.logger.info(f"Successfully added to ZIP: {file_path}")
+                        logger.info(f"Successfully added to ZIP: {file_path}")
                 else:
                     zip_file.write(backup_path, os.path.basename(backup_path))
-                    self.logger.info(f"Successfully added file to ZIP: {backup_path}")
+                    logger.info(f"Successfully added file to ZIP: {backup_path}")
 
             # deletes the original file/directory after compression
             if os.path.isdir(backup_path):
@@ -143,9 +210,9 @@ class LocalBackupManager:
             elif os.path.isfile(backup_path):
                 os.remove(backup_path)
 
-            self.logger.info("Compression successful!")
+            logger.info("Compression successful!")
             return Path(zip_name)
         except Exception as e:
             message = f"Compression failed: {e}"
-            self.logger.error(message)
-            raise Exception(message)
+            logger.error(message)
+            return None
--- main.py
+++ main.py
@@ -2,42 +2,58 @@ import sys
 import argparse
 
 from google_auth_oauthlib.flow import google
-import google_drive_manager
 from backup_config import BackupConfig
 from local_backup_manager import LocalBackupManager
 from pathlib import Path
 
-def main():
+# cli argument parsing method
+def get_cmd_parser():
     parser = argparse.ArgumentParser(
         prog="File Backup Script",
-        description="File Backup Script || by mantot_123 || github.com/mantot-123"
-    )
-    parser.add_argument("-s", "--source_path", help="Specifies the source directory. Required.")
-    parser.add_argument("-d", "--dest_path", help="Specifies the destination directory where the file should be backed up. Required.")
-    parser.add_argument("-t", "--target_file", help="Specifies the target file located in the source directory to backup. If not specified, the script will backup the whole source directory.")
-    parser.add_argument("-b", "--backup_name", help="Sets a custom name for the backed up file. If blank, the script will automatically name it.")
-    parser.add_argument("-c", "--compress", help="Sets a flag to compress or not compress the file after copying. 0 - DON'T COMPRESS, 1 = COMPRESS. Default value will be 0.")
+        description="File Backup Script || by mantot_123 || github.com/mantot-123")
+    
+    # valid command line arguments here
+    parser.add_argument("-s", "--source_path", 
+        help="Specifies the source directory. Required.", 
+        default="",
+        nargs="?")
+    parser.add_argument("-d", "--dest_path", 
+        help="Specifies the destination directory where the file should be backed up. Required.", 
+        default="",
+        nargs="?")
+    parser.add_argument("-t", "--target_file", 
+        help="Specifies the target file located in the source directory to backup. If not specified, the script will backup the whole source directory.", 
+        default="",
+        nargs="?")
+    parser.add_argument("-b", "--backup_name", 
+        help="Sets a custom name for the backed up file. If blank, the script will automatically name it.", 
+        default="",
+        nargs="?")
+    parser.add_argument("-c", "--compress", 
+        help="Sets a flag to compress or not compress the file after copying. 0 - DON'T COMPRESS, 1 = COMPRESS. Default value will be 0.", 
+        default="0")
     parser.add_argument(
         "--cloud",
         nargs="*",
         choices=["google_drive"],
-        help=f"Specify a list of cloud providers to upload the file to after backup."
-    )
+        help=f"Specify a list of cloud providers to upload the file to after backup.")
     
     args = parser.parse_args()
+    return (parser, args)
+
+def main():
+    parser = get_cmd_parser()[0]
+    args = get_cmd_parser()[1]
 
     if len(sys.argv) < 1:
         parser.print_help()
         return
 
-    source_path = "" if not args.source_path else args.source_path
-    dest_path = "" if not args.dest_path else args.dest_path
-    target_file = "" if not args.target_file else args.target_file
-    backup_name = "" if not args.backup_name else args.backup_name
-
-    compress_backup = "" if not args.compress else args.compress
-    compress_backup = "0" if compress_backup != "1" else "1"
-
+    source_path = args.source_path
+    dest_path = args.dest_path
+    target_file = args.target_file
+    backup_name = args.backup_name
+    compress_backup = args.compress
     cloud = args.cloud
 
     if not source_path:
@@ -52,21 +68,6 @@ def main():
     print("Performing backup...")
     backup = manager.perform_backup()
 
-    if backup != None:
-        if "google_drive" in cloud:
-            creds = google_drive_manager.authorise()
-            google_drive_manager.upload(creds, backup)
-            print("Backing up to Google Drive...")
-
-        if "onedrive" in cloud:
-            pass
-        
-        if "dropbox" in cloud:
-            pass
-
-        print(f"Backup successfully created. The output file is located locally at: {str(backup)}")
-    else:
-        print("Backup failed. Please check that the source file exists and try again")
 
 if __name__ == "__main__":
     main()
